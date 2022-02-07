299ba75ef5a89216dc69861029d584ae32ca8d06
Sicret89 <kamil.banas787@gmail.com>
2022-02-07 08:17:06 +0000

    static changed

--- google_books_api/api/settings.py
+++ google_books_api/api/settings.py
@@ -125,6 +125,9 @@ REST_FRAMEWORK = {
 }
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
